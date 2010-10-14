4b3a2ccb3fd7e3983c2e2ead8126cbfc13004b6a
James Likier <jlikier@gmail.com>
2010-10-14 01:21:06 -0600

    Updated vector3f and added constructor to chunk.

--- src/blocklib/common/Vector3F.java
+++ src/blocklib/common/Vector3F.java
@@ -7,4 +7,16 @@ public class Vector3F {
 	public float x;
 	public float y;
 	public float z;
+	
+	public Vector3F(float x, float y, float z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public Vector3F clone()
+	{
+		return new Vector3F(x,y,z);
+	}
 }
--- src/blocklib/map/Chunk.java
+++ src/blocklib/map/Chunk.java
@@ -9,4 +9,15 @@ public class Chunk {
 	public Vector3F dimension;
 	
 	public Block[][][] blocks;
+	
+	public Chunk(Vector3F dimension)
+	{
+		Vector3F d = dimension.clone();
+		int x = (int)d.x;
+		int y = (int)d.y;
+		int z = (int)d.z;
+		
+		this.dimension = d;
+		this.blocks = new Block[x][y][z];
+	}
 }
