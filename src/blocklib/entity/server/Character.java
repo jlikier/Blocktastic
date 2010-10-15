936b2a36bb04ab2539fc149f6a260cb797516ee6
James Likier <jlikier@gmail.com>
2010-10-14 21:22:17 -0600

    Added server.Character class, per design specification.

---
+++ src/blocklib/entity/server/Character.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2010 - James Likier and Julian Hartline
+ */
+package blocklib.entity.server;
+
+import blocklib.entity.*;
+
+public class Character extends Entity{
+	public String name;
+	public int baseHealth;
+	public int baseMana;
+	//public Inventory inventory;
+	//public List<EntityModifier> modifiers;
+}
