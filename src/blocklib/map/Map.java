d1fb97efd173eb099f8015858e906360d155249a
James Likier <jlikier@gmail.com>
2010-10-14 21:11:23 -0600

    Added the Map, per design specification.

---
+++ src/blocklib/map/Map.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2010 - James Likier and Julian Hartline
+ */
+package blocklib.map;
+
+import blocklib.common.*;
+
+public class Map {
+	public Vector3F mapSize;
+	public Vector3F chunkSize;
+	public Chunk[][][] chunks;
+	public String name;
+}
