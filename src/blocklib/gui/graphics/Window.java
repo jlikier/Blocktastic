5ea3210bc1d7475e911261eccff644ea318cd736
James Likier <jlikier@gmail.com>
2010-10-15 02:56:11 -0600

    Fixed the rendering issue.  There was an invalid glEnable() call with GL_COLOR, which doesn't exist.  I also added in the aspect ratio to the perspective.

--- src/blocklib/gui/graphics/Window.java
+++ src/blocklib/gui/graphics/Window.java
@@ -27,8 +27,7 @@ public class Window {
 		kb = new KeyboardHandler();
 		windowed();
 		try {
-			//Display.create();
-			Display.create(new PixelFormat(0, 8, 0));
+			Display.create();
 			glInit();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -42,9 +41,27 @@ public class Window {
 	public void render() {
 		if (Display.isVisible()) {
 			processKeyboard();
-			renderImpl();
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			renderFace();
+			//renderImpl(); //commenting to resolve issue
 		}
 		Display.update();
+	}
+	
+	public void renderFace()
+	{
+		glPushMatrix();
+		
+		glBegin(GL_QUADS);
+		{
+			glVertex3f(-1, 1, -1);
+			glVertex3f(-1, -1, -1);
+			glVertex3f(1, -1, -1);
+			glVertex3f(1, 1, -1);
+		}
+		glEnd();
+		
+		glPopMatrix();
 	}
 	
 	public void renderImpl() {
@@ -132,26 +149,28 @@ public class Window {
 	
 
 	private void glInit() {
-		glEnable(GL_LIGHTING);
-		glEnable(GL_CULL_FACE);
+		//glEnable(GL_LIGHTING); // commenting to resolve issue
+		//glEnable(GL_CULL_FACE); // commenting to resolve issue
 	    glEnable(GL_DEPTH_TEST);
-	    glEnable(GL_COLOR);
-	    glEnable(GL_DOUBLEBUFFER);
+	    //glEnable(GL_COLOR); //commenting to resolve
+	    //glEnable(GL_DOUBLEBUFFER); // I read online that lwjgl defualts to double buffering
 	    
-	    glEnable(GL_LIGHT0);
-	    FloatBuffer position = BufferUtils.createFloatBuffer(4).put(new float[] {0F,0F,5F,0F});
-	    glLight(GL_LIGHT0, GL_POSITION, (FloatBuffer)position.flip());
+	    //glEnable(GL_LIGHT0); // commenting to resolve issue
+	    //FloatBuffer position = BufferUtils.createFloatBuffer(4).put(new float[] {0F,0F,5F,0F}); // commenting to resolve issue
+	    //glLight(GL_LIGHT0, GL_POSITION, (FloatBuffer)position.flip()); // commenting to resolve issue
 		
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		gluPerspective(45F, 1F, 0, 5);
+		//gluPerspective(45F, 1F, 0, 5); //commenting to resolve issue
+		gluPerspective(45F, mode.getWidth() / (float)mode.getHeight(), 1F, 128F);
+		glTranslatef(0,0,-32);
 		
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 		//glViewport(0, 0, mode.getWidth(), mode.getHeight());
 		//glTranslatef(1,0,0); //left
 		//glTranslatef(0,1,0); //down
-		glTranslatef(0,0,-5);
+		//glTranslatef(0,0,-5); // commenting to resolve issue
 		
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		Display.setVSyncEnabled(true);
