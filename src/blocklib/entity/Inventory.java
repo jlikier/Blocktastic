542064bda7221b7f47b0a3fd05841b1b9edde0e9
James Likier <jlikier@gmail.com>
2010-10-14 21:23:40 -0600

    Added Inventory class, per design specification.

---
+++ src/blocklib/entity/Inventory.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2010 - James Likier and Julian Hartline
+ */
+package blocklib.entity;
+
+public class Inventory {
+	public int width;
+	public int height;
+	//public Item[][] items;
+}
