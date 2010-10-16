1a76629060c7e8ac6b037302a68b6f95a85d4733
James Likier <jlikier@gmail.com>
2010-10-15 19:06:25 -0600

    Added some testing for vertex arrays.

--- src/blocklib/gui/graphics/Window.java
+++ src/blocklib/gui/graphics/Window.java
@@ -20,9 +20,10 @@ public class Window {
 	public KeyboardHandler kb;
 	private DisplayMode	mode;
 	boolean fullscreen;
-	
+	FloatBuffer vertices;
+
 	float angle;
-	
+
 	public Window() {
 		kb = new KeyboardHandler();
 		windowed();
@@ -33,20 +34,46 @@ public class Window {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public boolean shouldExit() {
 		return Display.isCloseRequested();
 	}
-	
+
 	public void render() {
 		if (Display.isVisible()) {
 			processKeyboard();
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			renderImpl(); 
+			//renderImpl(); 
+			//renderVertexArray();
+			renderMoarCubes();
 		}
 		Display.update();
 	}
-	
+
+	public void renderMoarCubes()
+	{
+		glPushMatrix();{
+			glTranslatef(-50,-50,0);
+			for(int x = 0; x < 100; x=x+5)
+			{
+				for(int y = 0; y < 100; y++)
+				{
+					glPushMatrix();{
+						glTranslatef(x,y,0);
+						renderVertexArray();
+					}glPopMatrix();
+				}
+			}
+		}glPopMatrix();
+	}
+	public void renderVertexArray()
+	{
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, 0, vertices);
+		glDrawArrays(GL_QUADS, 0, 12 * 5);
+		glDisableClientState(GL_VERTEX_ARRAY);
+	}
+
 	public void renderImpl() {
 		angle += 1;
 
@@ -97,18 +124,18 @@ public class Window {
 
 	private void processKeyboard() {
 		kb.handle();
-		
+
 		if (Keyboard.isKeyDown(Keyboard.KEY_M)) {
 			if (fullscreen) 
 				windowed();
 			else
 				fullscreen();
 		}
-		
+
 		while ( Mouse.next() );
 	}
-	
-	
+
+
 	public void fullscreen() {
 		fullscreen = true;
 		try {
@@ -118,7 +145,7 @@ public class Window {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void windowed() {
 		fullscreen = false;
 		try {
@@ -128,29 +155,31 @@ public class Window {
 			e.printStackTrace();
 		}
 	}
-	
+
 
 	private void glInit() {
-		glEnable(GL_LIGHTING); 
+		//glEnable(GL_LIGHTING); 
 		glEnable(GL_CULL_FACE);
-	    glEnable(GL_DEPTH_TEST);
-	    
-	    glEnable(GL_LIGHT0); 
-	    FloatBuffer position = BufferUtils.createFloatBuffer(4).put(new float[] {0F,0F,5F,0F}); 
-	    glLight(GL_LIGHT0, GL_POSITION, (FloatBuffer)position.flip()); 
-		
+		glEnable(GL_DEPTH_TEST);
+
+		//glEnable(GL_LIGHT0); 
+		//FloatBuffer position = BufferUtils.createFloatBuffer(4).put(new float[] {0F,0F,5F,0F}); 
+		//glLight(GL_LIGHT0, GL_POSITION, (FloatBuffer)position.flip()); 
+
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		gluPerspective(45F, mode.getWidth() / (float)mode.getHeight(), 1F, 128F);
-		glTranslatef(0,0,-32);
-		
+		glTranslatef(0,0,-125);
+
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		
+
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		Display.setVSyncEnabled(true);
+		
+		CreateVertexArray();
 	}
-	
+
 	private DisplayMode findDisplayMode(int width, int height, int bpp) throws LWJGLException {
 		DisplayMode[] modes = Display.getAvailableDisplayModes();
 		for ( DisplayMode mode : modes ) {
@@ -161,8 +190,169 @@ public class Window {
 		}
 		return Display.getDesktopDisplayMode();
 	}
-	
+
 	public void cleanup() {
 		Display.destroy();
+	}
+	
+	public void CreateVertexArray()
+	{
+		vertices = BufferUtils.createFloatBuffer(3 * 4 * 6 * 5);
+		vertices.put(new float[] {
+				-1.0f, -1.0f, 1.0f,
+				1.0f, -1.0f, 1.0f,
+				1.0f, 1.0f, 1.0f,
+				-1.0f, 1.0f, 1.0f,
+
+				-1.0f, -1.0f, -1.0f,
+				-1.0f, 1.0f, -1.0f,
+				1.0f, 1.0f, -1.0f,
+				1.0f, -1.0f, -1.0f,
+
+				-1.0f, 1.0f, -1.0f,
+				-1.0f, 1.0f, 1.0f,
+				1.0f, 1.0f, 1.0f,
+				1.0f, 1.0f, -1.0f,
+
+				-1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f, 1.0f,
+				-1.0f, -1.0f, 1.0f,
+
+				1.0f, -1.0f, -1.0f,
+				1.0f, 1.0f, -1.0f,
+				1.0f, 1.0f, 1.0f,
+				1.0f, -1.0f, 1.0f,
+
+				-1.0f, -1.0f, -1.0f,
+				-1.0f, -1.0f, 1.0f,
+				-1.0f, 1.0f, 1.0f,
+				-1.0f, 1.0f, -1.0f,
+
+				//cube 2
+				1.0f, -1.0f, 1.0f,
+				3.0f, -1.0f, 1.0f,
+				3.0f, 1.0f, 1.0f,
+				1.0f, 1.0f, 1.0f,
+				
+				1.0f, -1.0f, -1.0f,
+				1.0f, 1.0f, -1.0f,
+				3.0f, 1.0f, -1.0f,
+				3.0f, -1.0f, -1.0f,
+				
+				1.0f, 1.0f, -1.0f,
+				1.0f, 1.0f, 1.0f,
+				3.0f, 1.0f, 1.0f,
+				3.0f, 1.0f, -1.0f,
+				
+				1.0f, -1.0f, -1.0f,
+				3.0f, -1.0f, -1.0f,
+				3.0f, -1.0f, 1.0f,
+				1.0f, -1.0f, 1.0f,
+				
+				3.0f, -1.0f, -1.0f,
+				3.0f, 1.0f, -1.0f,
+				3.0f, 1.0f, 1.0f,
+				1.0f, -1.0f, 1.0f,
+				
+				1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f, 1.0f,
+				1.0f, 1.0f, 1.0f,
+				1.0f, 1.0f, -1.0f,
+				
+				//cube 3
+				3.0f, -1.0f, 1.0f,
+				5.0f, -1.0f, 1.0f,
+				5.0f, 1.0f, 1.0f,
+				3.0f, 1.0f, 1.0f,
+				
+				3.0f, -1.0f, -1.0f,
+				3.0f, 1.0f, -1.0f,
+				5.0f, 1.0f, -1.0f,
+				5.0f, -1.0f, -1.0f,
+				
+				3.0f, 1.0f, -1.0f,
+				3.0f, 1.0f, 1.0f,
+				5.0f, 1.0f, 1.0f,
+				5.0f, 1.0f, -1.0f,
+				
+				3.0f, -1.0f, -1.0f,
+				5.0f, -1.0f, -1.0f,
+				5.0f, -1.0f, 1.0f,
+				3.0f, -1.0f, 1.0f,
+				
+				5.0f, -1.0f, -1.0f,
+				5.0f, 1.0f, -1.0f,
+				5.0f, 1.0f, 1.0f,
+				3.0f, -1.0f, 1.0f,
+				
+				3.0f, -1.0f, -1.0f,
+				3.0f, -1.0f, 1.0f,
+				3.0f, 1.0f, 1.0f,
+				3.0f, 1.0f, -1.0f,
+				
+				// cube 4
+				5.0f, -1.0f, 1.0f,
+				7.0f, -1.0f, 1.0f,
+				7.0f, 1.0f, 1.0f,
+				5.0f, 1.0f, 1.0f,
+				
+				5.0f, -1.0f, -1.0f,
+				5.0f, 1.0f, -1.0f,
+				7.0f, 1.0f, -1.0f,
+				7.0f, -1.0f, -1.0f,
+				
+				5.0f, 1.0f, -1.0f,
+				5.0f, 1.0f, 1.0f,
+				7.0f, 1.0f, 1.0f,
+				7.0f, 1.0f, -1.0f,
+				
+				5.0f, -1.0f, -1.0f,
+				7.0f, -1.0f, -1.0f,
+				7.0f, -1.0f, 1.0f,
+				5.0f, -1.0f, 1.0f,
+				
+				7.0f, -1.0f, -1.0f,
+				7.0f, 1.0f, -1.0f,
+				7.0f, 1.0f, 1.0f,
+				5.0f, -1.0f, 1.0f,
+				
+				5.0f, -1.0f, -1.0f,
+				5.0f, -1.0f, 1.0f,
+				5.0f, 1.0f, 1.0f,
+				5.0f, 1.0f, -1.0f,
+				
+				//cube 5
+				5.0f, -1.0f, 1.0f,
+				7.0f, -1.0f, 1.0f,
+				7.0f, 1.0f, 1.0f,
+				5.0f, 1.0f, 1.0f,
+				
+				5.0f, -1.0f, -1.0f,
+				5.0f, 1.0f, -1.0f,
+				7.0f, 1.0f, -1.0f,
+				7.0f, -1.0f, -1.0f,
+				
+				5.0f, 1.0f, -1.0f,
+				5.0f, 1.0f, 1.0f,
+				7.0f, 1.0f, 1.0f,
+				7.0f, 1.0f, -1.0f,
+				
+				5.0f, -1.0f, -1.0f,
+				7.0f, -1.0f, -1.0f,
+				7.0f, -1.0f, 1.0f,
+				5.0f, -1.0f, 1.0f,
+				
+				7.0f, -1.0f, -1.0f,
+				7.0f, 1.0f, -1.0f,
+				7.0f, 1.0f, 1.0f,
+				5.0f, -1.0f, 1.0f,
+				
+				5.0f, -1.0f, -1.0f,
+				5.0f, -1.0f, 1.0f,
+				5.0f, 1.0f, 1.0f,
+				5.0f, 1.0f, -1.0f
+		});
+		vertices.flip();
 	}
 }
