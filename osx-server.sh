b635a9d62de7186875015787f73bc783bfe27ff0
James Likier <jlikier@gmail.com>
2010-10-16 12:42:26 -0600

    Added slick-util, updated the run scripts, and added a texture.

--- osx-server.sh
+++ osx-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp "bin:lib/lwjgl-2.5/jar/*:" -Djava.library.path="lib/lwjgl-2.5/native/macosx" blocktastic.server.Server
+java -cp "bin:lib/lwjgl-2.5/jar/*:lib/slick/*" -Djava.library.path="lib/lwjgl-2.5/native/macosx" blocktastic.server.Server
