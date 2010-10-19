b48f85fba802dbe2bca6e946acb7f4e6e3905436
James Likier <jlikier@gmail.com>
2010-10-19 00:05:09 -0600

    Got some fast rendering.

--- src/blocklib/gui/graphics/rendering/MapRenderer.java
+++ src/blocklib/gui/graphics/rendering/MapRenderer.java
@@ -3,33 +3,13 @@ package blocklib.gui.graphics.rendering;
 import blocklib.common.*;
 import blocklib.map.*;
 import java.util.*;
-import java.nio.*;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.*;
-import static org.lwjgl.opengl.GL11.*;
 
 public class MapRenderer {
 	public HashMap<String, OptimizedChunk> chunks;
-	public FloatBuffer vertices;
-	public int verticesSize;
-	public Vector3I chunkSize;
-	public int chunksToBatch;
-	public int vboid;
 	
 	public MapRenderer(Vector3I chunkSize)
 	{
-		this.chunksToBatch = 16 * 16;
-		this.chunkSize = chunkSize.clone();
-		this.verticesSize = chunkSize.x * chunkSize.y * chunkSize.z * chunksToBatch * 3 * 4 * 6;
-		this.vertices = BufferUtils.createFloatBuffer(verticesSize);
 		this.chunks = new HashMap<String, OptimizedChunk>();
-		this.vboid = -1;
-		createVBO();
-	}
-	
-	public void createVBO()
-	{
-		vboid = ARBVertexBufferObject.glGenBuffersARB();
 	}
 	
 	public void addChunk(Chunk chunk, Vector3I position)
@@ -43,70 +23,9 @@ public class MapRenderer {
 	
 	public void render()
 	{
-		int totalFaces = 0;
-		if(vboid>0)
+		for(OptimizedChunk c : chunks.values())
 		{
-			int count = 0;
-			int faces = 0;
-			
-			// Clear the buffer
-			vertices.clear();
-			
-			// Fill the vbo with data
-			for(OptimizedChunk c : chunks.values())
-			{
-				count++;
-				
-				for(QuadFace f : c.faces)
-				{
-					totalFaces++;
-					faces++;
-					//System.out.println("Face");
-					//System.out.println(f.v1);
-					//System.out.println(f.v2);
-					//System.out.println(f.v3);
-					//System.out.println(f.v4);
-					//System.out.println("End Face");
-					vertices.put(f.v1.x);
-					vertices.put(f.v1.y);
-					vertices.put(f.v1.z);
-					vertices.put(f.v2.x);
-					vertices.put(f.v2.y);
-					vertices.put(f.v2.z);
-					vertices.put(f.v3.x);
-					vertices.put(f.v3.y);
-					vertices.put(f.v3.z);
-					vertices.put(f.v4.x);
-					vertices.put(f.v4.y);
-					vertices.put(f.v4.z);
-				}
-				
-				if(count > chunksToBatch)
-				{
-					// We have our buffer ready for rendering
-					vertices.flip();
-					renderVBO(faces * 3 * 4);
-					vertices.clear();
-					count = 0;
-					faces = 0;
-				}
-			}
-			if(count > 0)
-			{
-				// We have our buffer ready for rendering
-				vertices.flip();
-				renderVBO(faces * 3 * 4);
-			}
-			System.out.println("Total Faces: " + totalFaces);
+			c.render();
 		}
-	}
-	public void renderVBO(int size)
-	{
-		ARBVertexBufferObject.glBindBufferARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, vboid);
-		ARBVertexBufferObject.glBufferDataARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, vertices, ARBVertexBufferObject.GL_DYNAMIC_DRAW_ARB);
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glVertexPointer(3, GL_FLOAT, 0, 0);
-		glDrawArrays(GL_QUADS, 0, size);
-		glDisableClientState(GL_VERTEX_ARRAY);
 	}
 }
--- src/blocklib/gui/graphics/rendering/OptimizedChunk.java
+++ src/blocklib/gui/graphics/rendering/OptimizedChunk.java
@@ -3,24 +3,55 @@ package blocklib.gui.graphics.rendering;
 import java.util.*;
 import blocklib.common.*;
 import blocklib.map.*;
+import java.util.*;
+import java.nio.*;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.*;
+import static org.lwjgl.opengl.GL11.*;
 
 public class OptimizedChunk {
 	public List<QuadFace> faces;
+	public int vboid;
+	public FloatBuffer vertices;
+	public int verticecount;
 	
 	public OptimizedChunk()
 	{
 		faces = new ArrayList<QuadFace>();
+		vboid = ARBVertexBufferObject.glGenBuffersARB();
 	}
 	public OptimizedChunk(Chunk c, Vector3I offset)
 	{
 		this();
+		vertices = BufferUtils.createFloatBuffer(c.chunkSize.x * c.chunkSize.y * c.chunkSize.z * 3 * 4 * 6);
+		verticecount = 0;
 		generateFaces(c, offset);
+	}
+	public void render()
+	{
+		ARBVertexBufferObject.glBindBufferARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, vboid);
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, GL_FLOAT, 0, 0);
+		glDrawArrays(GL_QUADS, 0, verticecount * 3);
+		glDisableClientState(GL_VERTEX_ARRAY);
+	}
+	public void bufferData()
+	{
+		ARBVertexBufferObject.glBindBufferARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, vboid);
+		ARBVertexBufferObject.glBufferDataARB(ARBVertexBufferObject.GL_ARRAY_BUFFER_ARB, vertices, ARBVertexBufferObject.GL_DYNAMIC_DRAW_ARB);
+	}
+	public void putVertice(Vector3F v)
+	{
+		vertices.put(v.x);
+		vertices.put(v.y);
+		vertices.put(v.z);
 	}
 	
 	public void generateFaces(Chunk c, Vector3I offset)
 	{
 		Block b, temp;
 		Vector3I cursor, test;
+		QuadFace f;
 		for(int x = 0; x < c.chunkSize.x; x++)
 		{
 			for(int y = 0; y < c.chunkSize.y; y++)
@@ -36,35 +67,79 @@ public class OptimizedChunk {
 						test = cursor.add(0,1,0);
 						temp = c.getBlock(cursor);
 						if(!c.validPosition(test) || temp == null)
-							faces.add(BlockFace.topFace.offset(offset.add(x, y, z)));
+						{
+							f = BlockFace.topFace.offset(offset.add(x, y, z));
+							putVertice(f.v1);
+							putVertice(f.v2);
+							putVertice(f.v3);
+							putVertice(f.v4);
+							verticecount+=4;
+						}
 						//bottom
 						test = cursor.add(0,-1, 0);
 						temp = c.getBlock(test);
 						if(!c.validPosition(test) || temp == null)
-							faces.add(BlockFace.bottomFace.offset(offset.add(x, y, z)));
+						{
+							f = BlockFace.bottomFace.offset(offset.add(x, y, z));
+							putVertice(f.v1);
+							putVertice(f.v2);
+							putVertice(f.v3);
+							putVertice(f.v4);
+							verticecount+=4;
+						}
 						//front
 						test = cursor.add(0,0,1);
 						temp = c.getBlock(test);
 						if(!c.validPosition(test) || temp == null)
-							faces.add(BlockFace.frontFace.offset(offset.add(x, y, z)));
+						{
+							f = BlockFace.frontFace.offset(offset.add(x, y, z));
+							putVertice(f.v1);
+							putVertice(f.v2);
+							putVertice(f.v3);
+							putVertice(f.v4);
+							verticecount+=4;
+						}
 						//back 
 						test = cursor.add(0,0,-1);
 						temp = c.getBlock(test);
 						if(!c.validPosition(test) || temp == null)
-							faces.add(BlockFace.backFace.offset(offset.add(x, y, z)));
+						{
+							f = BlockFace.backFace.offset(offset.add(x, y, z));
+							putVertice(f.v1);
+							putVertice(f.v2);
+							putVertice(f.v3);
+							putVertice(f.v4);
+							verticecount+=4;
+						}
 						//left
 						test = cursor.add(-1, 0, 0);
 						temp = c.getBlock(test);
 						if(!c.validPosition(test) || temp == null)
-							faces.add(BlockFace.leftFace.offset(offset.add(x, y, z)));
+						{
+							f = BlockFace.leftFace.offset(offset.add(x, y, z));
+							putVertice(f.v1);
+							putVertice(f.v2);
+							putVertice(f.v3);
+							putVertice(f.v4);
+							verticecount+=4;
+						}
 						//right
 						test = cursor.add(1, 0, 0);
 						temp = c.getBlock(test);
 						if(!c.validPosition(test) || temp == null)
-							faces.add(BlockFace.rightFace.offset(offset.add(x, y, z)));
+						{
+							f = BlockFace.rightFace.offset(offset.add(x, y, z));
+							putVertice(f.v1);
+							putVertice(f.v2);
+							putVertice(f.v3);
+							putVertice(f.v4);
+							verticecount+=4;
+						}
 					}
 				}
 			}
 		}
+		vertices.flip();
+		bufferData();
 	}
 }
